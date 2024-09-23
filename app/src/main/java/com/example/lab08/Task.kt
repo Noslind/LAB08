fd1b1310cec1269c4ad073e160aeeb7087c9d639
Dnilson Achahuanco Huarilloclla <dnilson.ah.7@gmail.com>
2024-09-23 14:07:24 -0500

    Crear el DAO
    Paso 2 del punto 3 Implementación del Modelo y Base de Datos

---
+++ app/src/main/java/com/example/lab08/Task.kt
@@ -0,0 +1,14 @@
+package com.example.lab08
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+class Task {
+    @Entity(tableName = "tasks")
+    data class Task(
+        @PrimaryKey(autoGenerate = true) val id: Int = 0,
+        @ColumnInfo(name = "description") val description: String,
+        @ColumnInfo(name = "is_completed") val isCompleted: Boolean = false
+    )
+
+}
